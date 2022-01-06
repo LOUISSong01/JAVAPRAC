1a97108387e5b112dc307d67e8e41d7bf5bcc6b5
Louis Song <flysky1959@gmail.com>
2022-01-06 18:28:51 +0900

    the first algorithm

---
+++ src/기본수학1_1712/Hotel2.java
@@ -0,0 +1,27 @@
+package 기본수학1_1712;
+
+import java.util.Scanner;
+
+public class Hotel2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int H = sc.nextInt();
+		int W = sc.nextInt();
+		int N = sc.nextInt();
+		int T = sc.nextInt();
+		
+		
+		//놓친 부분: N번 손님의 층 수를 구할 때 N%H
+		//N%H == 0이라면 그냥 H 
+		
+		//N/H = 엘베로부터 얼마나 떨어져 있나
+		// N=7 H=3, 2
+	
+		
+		
+
+	}
+
+}
